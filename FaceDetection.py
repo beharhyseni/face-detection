c79893fd3f1a7bbe9105415ee5644af0e5f2a8b4
Behar Hyseni <behar.hyseni@alumni.ubc.ca>
2018-10-09 11:35:38 -0700

    Implemented face detector using template (not finalized)

--- FaceDetection.py
+++ FaceDetection.py
@@ -7,6 +7,12 @@ import ncc
 
 # IMAGES' PATHS CONSTANTS
 STUDENTS_IMAGE = 'C:/Users/behar/Desktop/Face-Detection-in-a-Scaled-Representation/libs/faces/students.jpg'
+JUDY_BATS_IMAGE = 'C:/Users/behar/Desktop/Face-Detection-in-a-Scaled-Representation/libs/faces/judybats.jpg'
+FANS_IMAGE = 'C:/Users/behar/Desktop/Face-Detection-in-a-Scaled-Representation/libs/faces/fans.jpg'
+TREE_IMAGE = 'C:/Users/behar/Desktop/Face-Detection-in-a-Scaled-Representation/libs/faces/tree.jpg'
+FAMILY_IMAGE = 'C:/Users/behar/Desktop/Face-Detection-in-a-Scaled-Representation/libs/faces/family.jpg'
+TEMPLATE_IMAGE = 'C:/Users/behar/Desktop/Face-Detection-in-a-Scaled-Representation/libs/faces/template.jpg'
+
 
 
 
@@ -29,7 +35,7 @@ def MakePyramid(image, minsize):
     return pyramid_list
         
 
-pyramid = MakePyramid(STUDENTS_IMAGE, 4)
+pyramid = MakePyramid(JUDY_BATS_IMAGE, 20)
 
 # for im in pyramid:
 #     im.show()
@@ -66,10 +72,78 @@ def ShowPyramid(pyramid):
                
         image.paste(pyramid[i],(temp_width, temp_height))
         temp_width += pyramid[i].size[0]
+    return image
+
+
+def FindTemplate(pyramid, template, threshold):
     
-    image.show()
+    TEMPLATE_WIDTH = 15
+    image = pyramid[0]
+    image = image.convert('RGB') 
+    draw = ImageDraw.Draw(image)
+    with Image.open(template) as template:
+        x, y = template.size
+        x = TEMPLATE_WIDTH
+        y = (float(TEMPLATE_WIDTH)/template.width) * template.height
+        template = template.resize((int(x),int(y)), Image.BICUBIC) 
+        
+        
+        for photo_index in range(0, len(pyramid)):       
+            
+            print photo_index
+            output = ncc.normxcorr2D(pyramid[photo_index], template)
+ 
+            comparison = np.where(output>threshold)
+            row = comparison[1]
+            column = comparison[0]
+            
+            for i,j in enumerate(row):       
+                        
+                rescaled_x = x * 1/(0.75**photo_index)
+                rescaled_y = y * 1/(0.75**photo_index)  
+                                              
+                M_x = j
+                M_y = column[i]
+                
+                
+                # THE FACE DETECTION RECTANGLE BOX
+                # Bottom line
+                x1 = M_x        - rescaled_x  
+                y1 = M_y        + rescaled_y   
+                x2 = M_x        + rescaled_x        
+                y2 = M_y        + rescaled_y               
+                draw.line((x1,y1,x2,y2),fill="red",width=2)
+                
+                # Right line
+                x1 = M_x        + rescaled_x        
+                y1 = M_y        + rescaled_y     
+                x2 = M_x        + rescaled_x       
+                y2 = M_y        - rescaled_y                  
+                draw.line((x1,y1,x2,y2),fill="red",width=2)
+                
+                # Top line
+                x1 = M_x        - rescaled_x
+                y1 = M_y        - rescaled_y
+                x2 = M_x        + rescaled_x
+                y2 = M_y        - rescaled_y                  
+                draw.line((x1,y1,x2,y2),fill="red",width=2)
+                
+                # Left Line
+                x1 = M_x        - rescaled_x 
+                y1 = M_y        + rescaled_y 
+                x2 = M_x        - rescaled_x 
+                y2 = M_y        - rescaled_y                        
+                draw.line((x1,y1,x2,y2),fill="red",width=2)
+                            
+            
+    return image
     
-ShowPyramid(pyramid)
+   
+    
+    
+FindTemplate(pyramid, TEMPLATE_IMAGE, 0.6).show()
+    
+    
     
 
 
